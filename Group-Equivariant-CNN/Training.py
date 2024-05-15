3b27e681c47bb022fb305d52d11810345f88d0b4
Alex Str <max.strunk32@gmail.com>
2024-05-15 07:39:15 +0200

    Improved the training code by introducig a loop over all the models.

--- Group-Equivariant-CNN/Training.py
+++ Group-Equivariant-CNN/Training.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import numpy as np
 from Models import CNN_1, CNN_2, CNN_3
 from pathlib import Path
+from wslpath import wslpath
 from torch.utils.data import DataLoader
 from torchvision import transforms, datasets
 
@@ -18,75 +19,116 @@ CNN_1_Model.to(device)
 CNN_2_Model.to(device)
 CNN_3_Model.to(device)
 
-# Define the current directory
+# Create a list of all CNN Models
+CNN_Models = [CNN_1_Model, CNN_2_Model, CNN_3_Model]
+
+# Determine the hyperparameters for the training
+batch_size = 32
+epochs = 3
+learning_rate = 0.01
+
+
+# Specify the current directory
 curr_dir = os.path.dirname(os.path.realpath(__file__))
+
+# Specify the path where the training data is stored
+train_data_dir = curr_dir + "/Data/Train"
+train_data_path = Path(train_data_dir)
+
+# Define the data-transformer to convert the image data into PyTorch Tensors of a specified dimension
+data_transformer = transforms.Compose([
+    transforms.Resize(size=(256,256)), #Resize the image to 256x256
+    transforms.ToTensor()
+])
+
+# Import the training data either as path on a Windows or a Linux Subsystem Path
+try:
+    train_data = datasets.ImageFolder(root=wslpath(train_data_path), transform=data_transformer, target_transform=None)
+except:
+    train_data = datasets.ImageFolder(root=train_data_path, transform=data_transformer, target_transform=None)
+
+# Create a train DataLoader:
+train_dataloader = DataLoader(dataset=train_data, batch_size=batch_size,shuffle=True)
 
 # Check whether a model directory already exists, if not creat one
 model_directory = curr_dir + "/Model_Parameters"
 model_dir_path = Path(model_directory)
-model_1_path = model_directory + "\CNN_1_Model"
-model_2_path = model_directory + "\CNN_2_Model"
-model_3_path = model_directory + "\CNN_3_Model"
 
-if not os.path.isdir(model_dir_path):
-    model_dir_path.mkdir(parents=True, exist_ok=True)
+# This is included in case this is run on a Linux Subsystem 
+try:
+    if not os.path.isdir(wslpath(model_dir_path)):
+        wslpath(model_dir_path).mkdir(parents=True, exist_ok=True)
+except:
+    if not os.path.isdir(model_dir_path):
+        model_dir_path.mkdir(parents=True, exist_ok=True)
 
-# Check whether a trained model already exists, if not start training 
-if not os.path.isfile(model_1_path):
+# Check whether a directory for the training losses already exists, if not creat one
+model_loss_directory = curr_dir + "/Model_Losses"
+model_loss_path = Path(model_loss_directory)
 
-    # Determine the hyperparameters for the training
-    batch_size = 32
-    epochs = 10
-    learning_rate = 0.001
-    
-    # Specify the path where the training data is stored
-    train_data_dir = curr_dir + "\Data\Train"
-    train_data_path = Path(train_data_dir)
-    print(train_data_path)
+# This is included in case this is run on a Linux Subsystem 
+try:
+    if not os.path.isdir(wslpath(model_loss_path)):
+        wslpath(model_loss_path).mkdir(parents=True, exist_ok=True)
+except:
+    if not os.path.isdir(model_loss_path):
+        model_loss_path.mkdir(parents=True, exist_ok=True)
 
-    # Define the data-transformer to convert the image data into PyTorch Tensors of a specified dimension
-    data_transformer = transforms.Compose([
-        transforms.Resize(size=(256,256)), #Resize the image to 256x256
-        transforms.ToTensor()
-    ])
+# Start the training for all Models in CNN_Models if there does not exist an already trained one.
+for i in range(len(CNN_Models)):
 
-    # Create the training data
-    train_data = datasets.ImageFolder(root=train_data_dir, transform=data_transformer, target_transform=None)
+    # Check whether a trained model 1 already exists, if not start training
+    model_exists = False 
+    model_path = model_directory + f"\CNN_{i+1}_Model"
+    try:
+        if not os.path.isfile(wslpath(model_path)):
+            model_exists = True
+    except:
+        if not os.path.isfile(model_path):
+            model_exists = True
 
-    # Create a train DataLoader:
-    train_dataloader = DataLoader(dataset=train_data, batch_size=batch_size,shuffle=True)
+    if not model_exists:
 
-    # Define the optimizer and the loss function and create a loss tracking list
-    optimizer = torch.optim.Adam(CNN_1_Model.parameters(), lr=learning_rate)
-    criterion = nn.CrossEntropyLoss()
-    loss_tracker = []
+        # Define the optimizer and the loss function and create a loss tracking list
+        optimizer = torch.optim.Adam(CNN_Models[i].parameters(), lr=learning_rate)
+        criterion = nn.CrossEntropyLoss()
+        loss_tracker = []
 
-    # Define the training loop
-    for epoch in range(epochs):
-        for i, (images, targets) in enumerate(train_dataloader):
+        # Define the training loop
+        for epoch in range(epochs):
+            for i, (images, targets) in enumerate(train_dataloader):
+                
+                # Push the images and targets to the device
+                images = images.to(device)
+                targets = targets.to(device)
+
+                # Forward pass
+                outputs = CNN_1_Model.forward(images)
+                loss = criterion(outputs, targets)
+                loss_tracker.append(loss.cpu().detach().numpy())
+
+                # Backward pass
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
             
-            # Push the images and targets to the device
-            images = images.to(device)
-            targets = targets.to(device)
-
-            # Forward pass
-            outputs = CNN_1_Model.forward(images)
-            loss = criterion(outputs, targets)
-            loss_tracker.append(loss.cpu().detach().numpy())
-
-            # Backward pass
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+            # Report over the training progress after certain epoch
+            print(f"CNN_{i+1}_Model: In epoch {epoch+1} with loss = {loss.item()}.")
         
-        # Report over the training progress after certain epoch
-        print(f"CNN_1_Model: In epoch {epoch+1} with loss = {loss.item()}.")
-    
-    torch.save(CNN_1_Model.state_dict(), model_1_path)
-    
+        torch.save(CNN_Models[i].state_dict(), model_path)
 
 
-if not os.path.isfile(model_2_path):
+'''
+# Check whether a trained model 2 already exists
+model_2_exists = False 
+try:
+    if not os.path.isfile(wslpath(model_2_path)):
+        model_2_exists = True
+except:
+    if not os.path.isfile(model_2_path):
+        model_2_exists = True
+
+if not model_2_exists:
 
     # Determine the hyperparameters for the training
     batch_size = 16
@@ -94,9 +136,8 @@ if not os.path.isfile(model_2_path):
     learning_rate = 0.12
     
     # Specify the path where the training data is stored
-    train_data_dir = curr_dir + "\Data\Train"
+    train_data_dir = curr_dir + "/Data/Train"
     train_data_path = Path(train_data_dir)
-    print(train_data_path)
 
     # Define the data-transformer to convert the image data into PyTorch Tensors of a specified dimension
     data_transformer = transforms.Compose([
@@ -104,8 +145,11 @@ if not os.path.isfile(model_2_path):
         transforms.ToTensor()
     ])
 
-    # Create the training data
-    train_data = datasets.ImageFolder(root=train_data_dir, transform=data_transformer, target_transform=None)
+    # Create the training data either as path on a Windows or a Linux Subsystem Path
+    try:
+        train_data = datasets.ImageFolder(root=wslpath(train_data_path), transform=data_transformer, target_transform=None)
+    except:
+        train_data = datasets.ImageFolder(root=train_data_path, transform=data_transformer, target_transform=None)
 
     # Create a train DataLoader:
     train_dataloader = DataLoader(dataset=train_data, batch_size=batch_size,shuffle=True)
@@ -145,8 +189,16 @@ if not os.path.isfile(model_2_path):
 
 
 
-# Check whether a trained model already exists, if not start training 
-if not os.path.isfile(model_3_path):
+# Check whether a trained model 3 already exists, if not start training 
+model_3_exists = False 
+try:
+    if not os.path.isfile(wslpath(model_1_path)):
+        model_3_exists = True
+except:
+    if not os.path.isfile(model_1_path):
+        model_3_exists = True
+
+if not model_3_exists:
 
     # Determine the hyperparameters for the training
     batch_size = 32
@@ -154,9 +206,9 @@ if not os.path.isfile(model_3_path):
     learning_rate = 0.01
     
     # Specify the path where the training data is stored
-    train_data_dir = curr_dir + "\Data\Train"
+    train_data_dir = curr_dir + "/Data/Train"
     train_data_path = Path(train_data_dir)
-    print(train_data_path)
+    
 
     # Define the data-transformer to convert the image data into PyTorch Tensors of a specified dimension
     data_transformer = transforms.Compose([
@@ -164,8 +216,11 @@ if not os.path.isfile(model_3_path):
         transforms.ToTensor()
     ])
 
-    # Create the training data
-    train_data = datasets.ImageFolder(root=train_data_dir, transform=data_transformer, target_transform=None)
+    # Create the training data either as path on a Windows or a Linux Subsystem Path
+    try:
+        train_data = datasets.ImageFolder(root=wslpath(train_data_path), transform=data_transformer, target_transform=None)
+    except:
+        train_data = datasets.ImageFolder(root=train_data_path, transform=data_transformer, target_transform=None)
 
     # Create a train DataLoader:
     train_dataloader = DataLoader(dataset=train_data, batch_size=batch_size,shuffle=True)
@@ -197,4 +252,5 @@ if not os.path.isfile(model_3_path):
         print(f"CNN_3_Model: In epoch {epoch+1} with loss = {loss.item()}.")
     
     torch.save(CNN_3_Model.state_dict(), model_3_path)
-    
+
+'''
